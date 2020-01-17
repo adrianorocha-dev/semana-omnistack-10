d637aea5e2578b5ee8be6f59cbe9869a043316b7
Adriano Rocha <adriano.olr@gmail.com>
2020-01-17 19:44:21 -0300

    Atualização em tempo real com WebSockets

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
 
 // index, show, store, update, destroy
 
@@ -37,6 +38,16 @@ module.exports = {
                 techs: techsArray,
                 location,
             });
+
+            // Filtrar as conexões que estão a no máximo 10km de distancia, e que o novo dev
+            // possua pelo menos uma das technologias filtradas
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude },
+                techsArray
+            );
+
+            console.log(sendSocketMessageTo);
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
         }
 
         res.json(dev);
